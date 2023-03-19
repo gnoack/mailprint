34b6dd4e7242a73a2184caca563f3992ef7e0400
Günther Noack <gnoack3000@gmail.com>
2023-03-19 07:42:42 +0100

    Move templates into their own files

--- render.go
+++ render.go
@@ -1,6 +1,7 @@
 package mailprint
 
 import (
+	"embed"
 	"io"
 	"net/mail"
 	"strings"
@@ -8,96 +9,13 @@ import (
 	"time"
 )
 
-var tmpl = template.Must(template.New("").Funcs(template.FuncMap{
+//go:embed templates/*
+var templateFiles embed.FS
+
+var tmpl = template.Must(template.New("main").Funcs(template.FuncMap{
 	"processBody": processBody,
 	"escape":      groffEscape,
-}).Parse(`
-{{define "person" -}}
-  {{if .Name -}}
-    {{.Name | escape}} <{{.Address | escape}}>
-  {{- else -}}
-    {{.Address | escape}}
-  {{- end -}}
-{{end -}}
-
-{{- define "main" -}}
-.TITLE "{{.Subject | escape}}"
-.DOCTYPE    LETTER
-.PRINTSTYLE TYPESET
-.PAPER      {{.PageFormat}}
-.
-.T_MARGIN   2.5c
-.B_MARGIN   3c
-.
-.LS 11
-.
-.PAGENUM_POS BOTTOM CENTER
-.PAGENUM_HYPHENS OFF
-.
-.FOOTER_RECTO C "\*[PAGE#]"
-.DOCHEADER OFF
-.
-.QUOTE_INDENT 0
-.QUOTE_SIZE -4
-.
-.NEWCOLOR red         RGB #880000
-.NEWCOLOR green       RGB #008800
-.NEWCOLOR quotedmail  RGB #444488
-.
-.START
-.FAMILY T
-.PT_SIZE 9
-\# XXX No idea why this is needed... :-/
-.sp 0.0001c
-\# Header
-{{if .LogoPdfPath -}}
-.PDF_IMAGE -R {{.LogoPdfPath}} 48p 48p
-{{end -}}
-.sp |2.5c
-.SILENT
-.PAD "\*[ST1]\fBSubject:\fR\*[ST1X]\*[FWD 6p]\*[ST2]#\*[ST2X]\*[FWD 54p]"
-.SILENT OFF
-.ST 1 R
-.ST 2 L QUAD
-.TAB 1
-\fBSubject:\fR
-.TN
-{{.Subject | escape}}
-{{range .From -}}
-.TAB 1
-\fBFrom:\fR
-.TN
-{{block "person" .}}{{end}}
-{{end -}}
-{{range .To -}}
-.TAB 1
-\fBTo:\fR
-.TN
-{{block "person" .}}{{end}}
-{{end -}}
-{{range .Cc -}}
-.TAB 1
-\fBCc:\fR
-.TN
-{{block "person" .}}{{end}}
-{{end -}}
-{{if .Date -}}
-.TAB 1
-\fBDate:\fR
-.TN
-{{.Date}}
-{{end -}}
-.TQ
-.DRH
-.sp 2
-\# xxx why is this needed for the text body?
-.\" Body
-.QUOTE
-.CODE
-{{.TextBody | processBody}}
-.QUOTE OFF
-{{end -}}
-`))
+}).ParseFS(templateFiles, "templates/*.mom"))
 
 func Render(em *Email, pageFormat, logoPdf string, w io.Writer) error {
 	return tmpl.ExecuteTemplate(w, "main", struct {
